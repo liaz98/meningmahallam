b46b162bfec673a6f73924601f7f56b57475daa1
Azizabonu Israilova <azibonu19@gmail.com>
2021-12-03 22:54:37 +0500

    Vacancy is created

--- api/models.py
+++ api/models.py
@@ -36,3 +36,18 @@ class Property(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    salary = models.DecimalField(max_digits=5, decimal_places=2)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
