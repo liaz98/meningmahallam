7101b2f98cd161f5c106d936fd1c8dcaa6a1df36
Azizabonu Israilova <azibonu19@gmail.com>
2021-12-03 22:35:33 +0500

    Property is created

--- api/models.py
+++ api/models.py
@@ -21,3 +21,18 @@ class Neighborhood(models.Model):
     def __str__(self):
         return self.name
 
+
+class Property(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    district = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(null=True, default="avatar.svg")
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
