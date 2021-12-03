89ebe40b70d7bbf7b8387122167d246c70cb8bda
Azizabonu Israilova <azibonu19@gmail.com>
2021-12-03 22:15:32 +0500

    Neighborhood model is created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    district = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    length = models.IntegerField(blank=True, null=True)
+    square = models.FloatField()
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    chairman = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
